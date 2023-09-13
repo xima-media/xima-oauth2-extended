b0c64736c9e60a3f58c03a0b149f562a4d0114a3
Maik Schneider <mas@xima.de>
2023-09-13 09:36:44 +0200

    feat: add xima icon

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'xima-x' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:xima_oauth2_extended/Resources/Public/Icons/xima-x.svg',
+    ],
+];
